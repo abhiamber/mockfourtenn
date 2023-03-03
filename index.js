4c8b0bbc56f21dee2dbe4f2f98236709ec99a46d
abhiamber191 <abhiamber191@gmail.com>
2023-03-03 21:42:59 +0530

    temp

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+let conncet = require("./config/db");
+require("dotenv").config();
+let PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+let cors = require("cors");
+app.use(cors());
+
+app.get("/", async (req, res) => {
+  res.send("hurray14.....");
+});
+
+
+
+app.listen(PORT, async () => {
+  await conncet();
+  console.log("server is working");
+});
